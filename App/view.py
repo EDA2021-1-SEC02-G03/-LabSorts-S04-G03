cbc5e6b3ca0d82ebe737715f775ad3072968ea4c
Alejandro Alvarez <a.alvarezj@uniandes.edu.co>
2021-02-24 15:52:01 -0500

    Entrega Ejemplo – laboratorio 4

--- App/view.py
+++ App/view.py
@@ -68,6 +68,16 @@ def printAuthorData(author):
             print('Titulo: ' + book['title'] + '  ISBN: ' + book['isbn'])
     else:
         print('No se encontro el autor')
+def printResults(ord_books, sample=10):
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:")
+        i=1
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' +
+                  book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
 
 
 def printBestBooks(books):
@@ -117,7 +127,8 @@ while True:
         size = input("Indique tamaño de la muestra: ")
         result = controller.sortBooks(catalog, int(size))
         print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+                                          str(result[0]))
+        printResults(result[1])
 
     else:
         sys.exit(0)
--- README.md
+++ README.md
@@ -26,5 +26,6 @@ Este directorio contiene todo el código de base que se entrega para el funciona
     |--Algorithms: Implementación de los algoritmos vistos en el curso (por ejemplo algoritmos de ordenamiento)
 
     |-- Utils: Funciones auxiliares para el funcionamiento de los TADs y estructuras de datos
-
-
+Alejandro Alvarez
+202020208
+a.alvarezj@uniandes.edu.co
